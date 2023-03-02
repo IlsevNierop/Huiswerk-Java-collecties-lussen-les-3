6d11f3f549530242e4425063b93824c4476f2082
IlsevNierop <ilsevannierop@gmail.com>
2023-03-02 16:56:32 +0100

    tm bonus 1-4 af - incl verwijderen hashset en toevoegen arraylist en array

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
-import java.util.HashMap;
-import java.util.Random;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Scanner;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
         // Hier mag je je code scrijven voor de hoofd-opdracht
-
+        // Numeric omzetten naar Alphabetic
 
         Integer[] intList = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         String[] alpList = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen"};
@@ -18,7 +16,7 @@ public class Main {
 
         int inputNum = 0;
         while (playGame) {
-            System.out.println("Wil je spelen? Typ in J of N");
+            System.out.println("Wil je vertalen van cijfers naar alfabetische nummers? Typ in J of N");
             String playAgain = myScanner.nextLine();
             if (playAgain.charAt(0) == ('N') || playAgain.charAt(0) == ('n')) {
                 System.out.println("Dankjewel voor het spelen");
@@ -48,69 +46,136 @@ public class Main {
                 System.out.println("Dit is geen geldige keuze.");
             }
         }
+
+
+        //MASTERMIND bonus opdracht #1-4
+        //HashSet is niet nodig voor Mastermind, omdat de getallen niet uniek hoeven te zijn. En het geeft alle waardes gesorteerd terug - dat willen we niet.
+
+
+        int[] secretNumberList = randomNumberGenerator();
+
+        String stringNumber = setToStringConverter(secretNumberList);
+
+        System.out.println("Mastermind code: " + stringNumber); // deze moet uitgecomment worden als je het mastermind spel echt wilt spelen - anders weet je het antwoord al.
+        feedback(stringNumber);
+
     }
 
 
-//MASTERMIND
+    // Moet 4 unieke random nummers returnen - waarvan de nummers allemaal 9 of lager zijn.
+
+
+    //  Niet met HashSet gewerkt - omdat dat eigenlijk niet logisch is, want daar worden nummers gesorteerd. Daarom ArrayList en array methode gemaakt - ga nu gebruik maken van array.
+    public static int[] randomNumberGenerator() {
         Random rand = new Random();
+//        HashSet<Integer> set = new HashSet<>(); direct een array gemaakt, zodat de nummers niet gesorteerd worden door HashSet
+        int[] secretNumberList = new int[4];
+        for (int i = 0; i < 4; ) {
+            int randNum = rand.nextInt(0, 10); //origin included, bound excluded
 
-        HashSet<Integer> secretNumber = randomNumberGenerator(rand, secretNumber);
-        String stringNumber =  setToStringConverter(secretNumber);
-//
-//        System.out.println(stringNumber);
-//        feedback();
-
-
-
-        public static HashSet<Integer> randomNumberGenerator(Random rand, HashSet<Integer> secretNumber){
-
-            while (secretNumber.size() <4) {
-                int randNum = rand.nextInt(0, 9);
-
-                secretNumber.add(randNum);
-            }
-
-            return secretNumber;
-
-        }
-
-        public static String setToStringConverter(HashSet<Integer> secretNumber){
-
-            String[] secretNumerArray = secretNumber.toArray(new String[secretNumber.size()]);
-
-            for (int i = 0; i < secretNumber.size(); i++){
-                String stringNumber = "";
-                System.out.println(stringNumber);
-                stringNumber = stringNumber + (secretNumerArray[i].toString());
-                System.out.println(stringNumber);
-            }
-
-        }
-
-
-
-    /*
-     Deze methode is voor de bonus opdracht.
-     */
-    public static void feedback(String stringnumber) {
-        Scanner scanner = new Scanner(System.in);
-        StringBuilder feedback = new StringBuilder();
-        System.out.println("take a guess");
-        String guess = scanner.nextLine();
-        if (Objects.equals(guess, stringnumber)) {
-            System.out.println("gefeliciteerd je hebt het goed");
-        } else {
-            for (int i = 0; i < 4; i++) {
-                if (guess.substring(i, i + 1).equals(stringnumber.substring(i, i + 1))) {
-                    feedback.append("+");
-                } else if (stringnumber.contains(guess.substring(i, i + 1))) {
-                    feedback.append("0");
-                } else {
-                    feedback.append("X");
+            boolean unique = true;
+            for (int j = 0; j < i; j++) {
+                if (randNum == secretNumberList[j]) {
+                    unique = false;
+                    break;
                 }
             }
+            if (unique) {
+                secretNumberList[i] = randNum;
+                i++;
+            }
+
         }
-        System.out.println(feedback.toString());
+
+        return secretNumberList;
+    }
+
+
+
+//    public static ArrayList<Integer> randomNumberGenerator() {
+//        Random rand = new Random();
+////        HashSet<Integer> set = new HashSet<>(); direct een array gemaakt, zodat de nummers niet gesorteerd worden door HashSet
+//        ArrayList<Integer> secretNumberList = new ArrayList<>();
+//        while (secretNumberList.size() < 4) {
+//            for (int i = 0; i < 4; i++) {
+//                int randNum = rand.nextInt(0, 10); //origin included, bound excluded
+//                if (secretNumberList.size() < 1) {
+//                    secretNumberList.add(randNum);
+//                }
+//                else {
+//                    for (Integer j : secretNumberList) {
+//                        if (secretNumberList.contains(randNum)) {
+//                            continue;
+//                        } else {
+//                            secretNumberList.add(randNum);
+//                            break;
+//                        }
+//                    }
+//                }
+//
+//            }
+//        }
+//            System.out.println("List: " + secretNumberList);
+//        return secretNumberList;
+//    }
+
+    public static String setToStringConverter(int[] secretNumberList) {
+
+
+        String stringNumber = "";
+
+        for (int i = 0; i < secretNumberList.length; i++) {
+            stringNumber = stringNumber + (secretNumberList[i]);
+        }
+        return stringNumber;
+
+    }
+//
+//    /*
+//     Deze methode is voor de bonus opdracht.
+//     */
+//
+    public static void feedback(String stringNumber) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder feedback = new StringBuilder();
+        System.out.println("Kies een getal van 4 unieke cijfers om de mastermind code te raden.");
+        boolean continueTrying = true;
+        while (continueTrying) {
+            String guess = scanner.nextLine();
+            if (guess.length() != 4) {
+                System.out.println("Je antwoord moet bestaan uit 4 cijfers, niet meer, niet minder. \n Probeer nogmaals: ");
+                continue;
+            } else if (Objects.equals(guess, stringNumber)) {
+                System.out.println("Gefeliciteerd je hebt het goed geraden");
+                continueTrying = false;
+                break;
+            } else {
+                for (int i = 0; i < 4; i++) {
+                    if (guess.substring(i, i + 1).equals(stringNumber.substring(i, i + 1))) {
+                        feedback.append("+");
+                    } else if (stringNumber.contains(guess.substring(i, i + 1))) {
+                        feedback.append("0");
+                    } else {
+                        feedback.append("X");
+                    }
+                }
+                System.out.println(feedback.toString());
+                System.out.println("Helaas, verkeerd geraden. \n + betekent dat het cijfer juist is en op de juiste plek staat. \n O betekent een juist cijfer, maar verkeerde plek. \n X betekent dat het cijfer er niet in zit. \nProbeer nogmaals:");
+                feedback = new StringBuilder();
+                continue;
+
+            }
+        }
     }
 }
+// einde bonus MASTERMIND
+
+// Bonus Poortwachter (extra opdracht van Paul):
+//Er is een poort en daarbij een poortwachter als je hierdoorheen wilt dan zegt de poortwachter een getal en jij moet een getal terug zeggen.
+//        # als jouw antwoord correct is mag je erdoorheen. Paul wilt door de poort de poortwachter zegt acht. paul antwoord met 4 en mag doorlopen.
+//        # Bas wil ook door de poort de poortwachter zegt twaalf en bas zegt 6.
+//        # jij wilt er ook doorheen de poortwachter zegt elf. wat moet je antwoorden?
+//        # jouw antwoord zou moeten zijn 3 want elf bestaat uit 3 letters.
+
+
 
